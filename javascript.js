26219b104e733ac042352a5d21c5f92ab07786b0
alicebnelson <nelsonaliceb@gmail.com>
2017-12-12 21:29:37 -0600

    Added Firebase key

--- javascript.js
+++ javascript.js
@@ -6,6 +6,18 @@
 
 /* Insert firebase API information here */
 
+var config = {
+    apiKey: "AIzaSyAmeCQpq3Gpvmncg2iRx1_p2Jd8yixEFew",
+    authDomain: "rqtz-51f2f.firebaseapp.com",
+    databaseURL: "https://rqtz-51f2f.firebaseio.com",
+    projectId: "rqtz-51f2f",
+    storageBucket: "rqtz-51f2f.appspot.com",
+    messagingSenderId: "252970490945"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
 /*insert Facebook/Instagram API information here */
 
 /*Function that takes in a name of an organization, connects and finds their social media account(s)
